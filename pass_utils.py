ad4c8439c5b661822276df5b15b4c487250d53f4
Evan Warner <evanwarner@Evans-MacBook-Air.local>
2025-05-22 17:27:21 +0200

    added through pass logic

--- pass_utils.py
+++ pass_utils.py
@@ -1,4 +1,4 @@
-from common_utils import cartesian_distance
+from common_utils import cartesian_distance, closest_coords
 
 def is_progressive_pass(passer_team, receiver_team, passer_x, passer_y, receiver_x, receiver_y):
     distance_change_from_right_goal = cartesian_distance(receiver_x, receiver_y, 1, 0.5) - cartesian_distance(passer_x, passer_y, 1, 0.5)
@@ -51,7 +51,29 @@ def is_penalty_area_pass(passer_team, receiver_team, passer_x, passer_y, receive
     return into_left_penalty_area or into_right_penalty_area
 
 
-def classify_pass(event, passer_team, receiver_team, passer_x, passer_y, receiver_x, receiver_y):
+def get_last_man_x(event_time, passer_team, bboxes):
+    # get the player with farthest x_coord from the defending team
+    min_x, max_x = None, None
+    for tracker_id in bboxes:
+        current_bboxes = bboxes[tracker_id]
+        if current_bboxes[0]["timestamp"] > event_time or current_bboxes[-1]["timestamp"] < event_time:
+            continue
+        current_coords = closest_coords(event_time, current_bboxes)
+        if current_coords["team"] != passer_team:
+            min_x = min(min_x, current_coords["x"])
+            max_x = max(max_x, current_coords["x"])
+    return max_x if passer_team == 0 else min_x
+    
+
+def is_through_pass(event, passer_team, bboxes, passer_x, receiver_x):
+    last_man_x = get_last_man_x(event["start_time"]+1, passer_team, bboxes)
+    return (
+        (passer_team == 0 and passer_x < last_man_x - 0.05 and receiver_x > last_man_x + 0.05) or # through pass to the right
+        (passer_team == 1 and passer_x > last_man_x - 0.05 and receiver_x < last_man_x + 0.05) # through pass to the left
+    )
+
+
+def classify_pass(event, passer_team, receiver_team, passer_x, passer_y, receiver_x, receiver_y, bboxes):
     # only successful passes have a tag
     if passer_team != receiver_team:
         return []
@@ -73,4 +95,6 @@ def classify_pass(event, passer_team, receiver_team, passer_x, passer_y, receive
         tags.append("final_third_pass")
     if is_penalty_area_pass(passer_team, receiver_team,passer_x, passer_y, receiver_x, receiver_y):
         tags.append("penalty_area_pass")
+    if is_through_pass(event, passer_team, bboxes):
+        tags.append("through_pass")
     return tags
